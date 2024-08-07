725fe6b08d3cf42750a187dce3b26f452ff0f282
BogdanShamanskyi <bogdanshamanskyi@gmail.com>
2024-08-07 16:56:33 +0300

    Hide site from indexing to admin

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,8 +3,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+{{--    <meta name="description" content="">--}}
+{{--    <meta name="author" content="">--}}
+    <meta name="robots" content="noindex, nofollow" />
 
     <title>Laramint ( Elearning & Gamification LMS )</title>
 
@@ -15,7 +16,7 @@
         rel="stylesheet"> --}}
 
     <!-- Custom styles for this template-->
-    @if(Auth::check() && Auth::user()->theme == 'dark')  
+    @if(Auth::check() && Auth::user()->theme == 'dark')
         <link href="{{ URL::to('css/dark/sb-admin-2.css') }}" rel="stylesheet">
     @else
         <link href="{{ URL::to('css/sb-admin-2.css') }}" rel="stylesheet">
